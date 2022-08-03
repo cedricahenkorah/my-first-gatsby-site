5913114928e359df02b428e311d3dd2efc1038a8
Cahenkorah <42823702+Cahenkorah@users.noreply.github.com>
2022-08-03 23:40:53 +0000

    new changes

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../components/layout";
+
+const AboutPage = () => {
+  return (
+    <Layout pageTitle="About me">
+      <p>Hello, i'm Cedric. A software developer...</p>
+    </Layout>
+  );
+};
+
+export default AboutPage;
