fc402c9f7ddd72662ab6a1818e1684269dfb2e14
Cahenkorah <42823702+Cahenkorah@users.noreply.github.com>
2022-08-04 15:25:51 +0000

    added home page photo

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import * as React from "react";
 import Layout from "../components/layout";
+import { StaticImage } from "gatsby-plugin-image";
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>Here we go!</p>
+      <StaticImage alt="picture" src="../images/pic1.jpg" />
     </Layout>
   );
 };
